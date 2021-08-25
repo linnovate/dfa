6ec92d86a4ebffc7f88483a1d9fabec7ea08a59b
Lior Kesos <lior@linnovate.net>
2021-08-25 15:31:30 +0300

    Addign drun.sh utility to hepl with the build, starting and deploying aspects

---
+++ drun.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# build daml
+
+case $1 in
+	build)
+		daml build
+		daml codegen js .daml/dist/dfa-0.1.0.dar -o ui/daml.js
+		# Build UI
+		cd ui
+		npm i
+		npm run-script build
+		;;
+
+	start)
+		echo `pwd`
+		echo " starting daml"
+		daml start &
+		echo "starting dev server"
+		cd ui && npm start
+		;;
+
+	hub)
+		# building daml
+		daml build -o target/create-daml-app.dar
+
+		#building the ui
+		daml codegen js target/create-daml-app.dar -o ui/daml.js
+		cd ui && npm install && npm run-script build
+		zip -r ../target/create-daml-app-ui.zip build
+		;;
+	*)
+		echo "No valid command sent - seeya!"
+		;;
+esac
